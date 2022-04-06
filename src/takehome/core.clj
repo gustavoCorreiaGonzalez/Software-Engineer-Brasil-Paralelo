e626385ac6727b6b8aef512857730c1ec90bacfd
gustavoCorreiaGonzalez <gustavogonzalez@alunos.utfpr.edu.br>
2022-04-05 23:14:26 -0300

    Finalizado o desenvolvimento e testes da função que valida se a assinatura está ativa

--- src/takehome/core.clj
+++ src/takehome/core.clj
@@ -1,6 +1,12 @@
 (ns takehome.core
   (:require [java-time :as time]))
 
+(defn active-subscription? [subscription]
+  (let [today time/local-date-time]
+    (time/before? (:subscription-start subscription)
+                  (today)
+                  (:subscription-end subscription))))
+
 (defn media-date-between-subscription-date? [midia subscription]
   (time/before? (:subscription-start subscription)
                 (:released-at midia)
@@ -8,14 +14,13 @@
 
 (defn patriota-access? [midia subscription]
   (or
-   (= (:type midia) :series)
-   (= (:type midia) :podcast)
-   (= (:type midia) :debate)
+  ;;  (.contains [:series :podcast :debate] (:type midia))
+   (contains? {:series nil :podcast nil :debate nil} (:type midia))
    (and
     (= (:type midia) :interview)
     (media-date-between-subscription-date? midia subscription))))
 
 (defn can-access? [midia subscription]
-  (if (= (:type midia) :series)
-    (and (= (:type subscription) :patriota)
-         (media-date-between-subscription-date? midia subscription))))
+  (and
+   (active-subscription? subscription)
+   (= (:type subscription) :patriota) (patriota-access? midia subscription)))
--- test/takehome/core_test.clj
+++ test/takehome/core_test.clj
@@ -4,26 +4,33 @@
             [takehome.core :as sub]))
 
 (deftest media-date-between-subscription-date-test
-  (are [result subscription] (= result
-                            (sub/media-date-between-subscription-date?
-                             {:released-at (time/local-date-time "2019-07-24T20:02:34.691")}
-                             subscription))
-    true  {:subscription-start (time/local-date-time "2019-01-24T11:46:22.811")
-           :subscription-end   (time/local-date-time "2020-01-24T11:46:22.811") }
-    false {:subscription-start (time/local-date-time "2017-01-24T11:46:22.811")
-           :subscription-end   (time/local-date-time "2019-01-24T11:46:22.811") }
-    false {:subscription-start (time/local-date-time "2020-01-24T08:30:00.154")
-           :subscription-end   (time/local-date-time "2021-01-24T08:30:00.154")})
-  (are [result subscription] (= result
-                                (sub/media-date-between-subscription-date?
-                                 {:released-at (time/local-date-time "2021-05-01T00:00:00.000")}
-                                 subscription))
-    false {:subscription-start (time/local-date-time "2019-01-24T11:46:22.811")
-           :subscription-end   (time/local-date-time "2020-01-24T11:46:22.811")}
-    false {:subscription-start (time/local-date-time "2017-01-24T11:46:22.811")
-           :subscription-end   (time/local-date-time "2019-01-24T11:46:22.811")}
-    false {:subscription-start (time/local-date-time "2020-01-24T08:30:00.154")
-           :subscription-end   (time/local-date-time "2021-01-24T08:30:00.154")}))
+  (are [result midia subscription] (= result
+                                      (sub/media-date-between-subscription-date?
+                                       midia
+                                       subscription))
+    true  {:released-at (time/local-date-time "2019-07-24T20:02:34.691")}
+    {:subscription-start (time/local-date-time "2019-01-24T11:46:22.811")
+     :subscription-end   (time/local-date-time "2020-01-24T11:46:22.811")}
+
+    false {:released-at (time/local-date-time "2019-07-24T20:02:34.691")}
+    {:subscription-start (time/local-date-time "2017-01-24T11:46:22.811")
+     :subscription-end   (time/local-date-time "2019-01-24T11:46:22.811")}
+
+    false {:released-at (time/local-date-time "2019-07-24T20:02:34.691")}
+    {:subscription-start (time/local-date-time "2020-01-24T08:30:00.154")
+     :subscription-end   (time/local-date-time "2021-01-24T08:30:00.154")}
+    
+    false {:released-at (time/local-date-time "2021-05-01T00:00:00.000")}
+    {:subscription-start (time/local-date-time "2019-01-24T11:46:22.811")
+     :subscription-end   (time/local-date-time "2020-01-24T11:46:22.811")}
+    
+    false {:released-at (time/local-date-time "2021-05-01T00:00:00.000")}
+    {:subscription-start (time/local-date-time "2017-01-24T11:46:22.811")
+     :subscription-end   (time/local-date-time "2019-01-24T11:46:22.811")}
+    
+    false {:released-at (time/local-date-time "2021-05-01T00:00:00.000")}
+    {:subscription-start (time/local-date-time "2020-01-24T08:30:00.154")
+     :subscription-end   (time/local-date-time "2021-01-24T08:30:00.154")}))
 
 (deftest patriota-access-test
   (are [result subscription] (= result
@@ -57,14 +64,30 @@
           :subscription-start (time/local-date-time "2020-02-15T14:36:27.567")
           :subscription-end   (time/local-date-time "2021-02-15T14:36:27.567")}))
 
-(deftest test-patriota
+(deftest active-subscription-test
+  (are [result subscription] (= result
+                                (sub/active-subscription?
+                                 subscription))
+    false  {:subscription-start (time/local-date-time "2019-01-24T11:46:22.811")
+           :subscription-end   (time/local-date-time "2020-01-24T11:46:22.811")}
+
+    false {:subscription-start (time/local-date-time "2017-01-24T11:46:22.811")
+           :subscription-end   (time/local-date-time "2019-01-24T11:46:22.811")}
+
+    false {:subscription-start (time/local-date-time "2021-01-24T08:30:00.154")
+           :subscription-end   (time/local-date-time "2022-01-24T08:30:00.154")}
+    
+    true {:subscription-start (time/local-date-time "2021-07-01T12:40:10.124")
+           :subscription-end   (time/local-date-time "2022-07-01T12:40:10.124")}))
+
+(deftest can-access-test
   (are [result purchase] (= result
                             (sub/can-access?
-                             {:type :series :name "1964: O Brasil entre Armas e Livros", :released-at (time/local-date-time "2019-07-24T20:02:34.691")}
+                             {:type :series :name "Brasil - A Última Cruzada", :released-at (time/local-date-time "2019-07-08T16:37:11.184")}
                              purchase))
-    true  {:type               :patriota
+    false  {:type               :patriota
            :subscription-start (time/local-date-time "2019-01-24T11:46:22.811")
            :subscription-end   (time/local-date-time "2020-01-24T11:46:22.811") }
-    false {:type               :patriota
-           :subscription-start (time/local-date-time "2017-01-24T11:46:22.811")
-           :subscription-end   (time/local-date-time "2019-01-24T11:46:22.811") }))
+    true {:type               :patriota
+           :subscription-start (time/local-date-time "2021-07-01T12:40:10.124")
+           :subscription-end   (time/local-date-time "2022-07-01T12:40:10.124") }))
