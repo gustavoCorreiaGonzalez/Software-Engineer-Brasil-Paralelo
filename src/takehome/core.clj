751f460502946c479b3038d11c54ecc152e8667a
gustavoCorreiaGonzalez <gustavogonzalez@alunos.utfpr.edu.br>
2022-04-05 22:17:19 -0300

    Finalizado o desenvolvimento e testes da verificação do acesso do usuário patriota

--- src/takehome/core.clj
+++ src/takehome/core.clj
@@ -6,7 +6,16 @@
                 (:released-at midia)
                 (:subscription-end subscription)))
 
+(defn patriota-access? [midia subscription]
+  (or
+   (= (:type midia) :series)
+   (= (:type midia) :podcast)
+   (= (:type midia) :debate)
+   (and
+    (= (:type midia) :interview)
+    (media-date-between-subscription-date? midia subscription))))
+
 (defn can-access? [midia subscription]
-  (if (= (:type midia) :movie)
+  (if (= (:type midia) :series)
     (and (= (:type subscription) :patriota)
          (media-date-between-subscription-date? midia subscription))))
--- test/takehome/core_test.clj
+++ test/takehome/core_test.clj
@@ -25,10 +25,42 @@
     false {:subscription-start (time/local-date-time "2020-01-24T08:30:00.154")
            :subscription-end   (time/local-date-time "2021-01-24T08:30:00.154")}))
 
+(deftest patriota-access-test
+  (are [result subscription] (= result
+                            (sub/patriota-access?
+                             {:type :series :name "1964: O Brasil entre Armas e Livros", :released-at (time/local-date-time "2019-07-24T20:02:34.691")}
+                             subscription))
+    true  {:type               :patriota
+           :subscription-start (time/local-date-time "2019-01-24T11:46:22.811")
+           :subscription-end   (time/local-date-time "2020-01-24T11:46:22.811")}
+    true {:type               :patriota
+           :subscription-start (time/local-date-time "2017-01-24T11:46:22.811")
+           :subscription-end   (time/local-date-time "2019-01-24T11:46:22.811")})
+  (are [result subscription] (= result
+                                (sub/patriota-access?
+                                 {:type :podcast :name "CORINGA - Podcast Cultura Paralela #1", :released-at (time/local-date-time "2020-03-29T20:02:34.337")}
+                                 subscription))
+    true  {:type               :patriota
+           :subscription-start (time/local-date-time "2019-01-24T11:46:22.811")
+           :subscription-end   (time/local-date-time "2020-01-24T11:46:22.811")}
+    true {:type               :patriota
+          :subscription-start (time/local-date-time "2020-02-15T14:36:27.567")
+          :subscription-end   (time/local-date-time "2021-02-15T14:36:27.567")})
+  (are [result subscription] (= result
+                                (sub/patriota-access?
+                                 {:type :interview :name "Congresso Brasil Paralelo - Rodrigo gurgel", :released-at (time/local-date-time "2019-01-24T11:45:57.229")}
+                                 subscription))
+    true  {:type               :patriota
+           :subscription-start (time/local-date-time "2019-01-01T11:46:22.811")
+           :subscription-end   (time/local-date-time "2020-01-24T11:46:22.811")}
+    false {:type               :patriota
+          :subscription-start (time/local-date-time "2020-02-15T14:36:27.567")
+          :subscription-end   (time/local-date-time "2021-02-15T14:36:27.567")}))
+
 (deftest test-patriota
   (are [result purchase] (= result
                             (sub/can-access?
-                             {:type :movie :name "1964: O Brasil entre Armas e Livros", :released-at (time/local-date-time "2019-07-24T20:02:34.691")}
+                             {:type :series :name "1964: O Brasil entre Armas e Livros", :released-at (time/local-date-time "2019-07-24T20:02:34.691")}
                              purchase))
     true  {:type               :patriota
            :subscription-start (time/local-date-time "2019-01-24T11:46:22.811")
